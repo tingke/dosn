d7b93abbdddf9c572a2d3e2635a43f3f6c00ffb7
liukun <liukun@dosing100.com>
2022-10-14 10:41:26 +0800

    feat: update

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -6,10 +6,13 @@ module.exports = [
         link: "/web/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
             // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-            { text: "Vue", link: "/pages/471299/" },
+            { text: "Vue2", link: "/pages/471299/" },
             { text: "JS", link: "/pages/23e8b8/" },
             { text: "HTML", link: "/pages/8309a5b876fc95e3/" },
             { text: "CSS", link: "/pages/c8f128/" },
+            { text: "ES6 笔记", link: "/pages/f344d070a1031ef7/" },
+            { text: "TypeScript 从零实现 axios", link: "/pages/e05dce83e5129785/" },
+            { text: "Git学习笔记", link: "/pages/8292d8/" },
             { text: "收藏", link: "/pages/beb6c0bd8a66cea6/" },
         ],
     },
