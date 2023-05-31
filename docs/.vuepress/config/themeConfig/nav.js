3db57751e9d11a218d9956cdc07f9f6b52c2019c
liukun <liukun@dosing100.com>
2023-05-31 16:31:53 +0800

    feat: update

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -6,13 +6,12 @@ module.exports = [
         link: "/web/", //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
             // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-            { text: "Vue2", link: "/pages/471299/" },
-            { text: "JS", link: "/pages/23e8b8/" },
             { text: "HTML", link: "/pages/8309a5b876fc95e3/" },
             { text: "CSS", link: "/pages/c8f128/" },
-            { text: "小程序", link: "/pages/df2429/" },
+            { text: "JavaScript", link: "/pages/23e8b8/" },
             { text: "ES6 笔记", link: "/pages/f344d070a1031ef7/" },
-            { text: "TypeScript 从零实现 axios", link: "/pages/e05dce83e5129785/" },
+            { text: "Vue2", link: "/pages/471299/" },
+            { text: "小程序", link: "/pages/df2429/" },
             { text: "Git学习笔记", link: "/pages/8292d8/" },
             { text: "收藏", link: "/pages/beb6c0bd8a66cea6/" },
         ],
@@ -20,14 +19,18 @@ module.exports = [
     {
         text: "开发规范", link: "/standard/"
     },
+    // {
+    //     text: "索引",
+    //     link: "/archives/",
+    //     items: [
+    //         { text: "分类", link: "/categories/" },
+    //         { text: "标签", link: "/tags/" },
+    //         { text: "归档", link: "/archives/" },
+    //     ],
+    // },
     {
-        text: "索引",
-        link: "/archives/",
-        items: [
-            { text: "分类", link: "/categories/" },
-            { text: "标签", link: "/tags/" },
-            { text: "归档", link: "/archives/" },
-        ],
+        text: "归档",
+        link: "/archives/"
     },
     { text: "关于", link: "/about/" },
 ];
